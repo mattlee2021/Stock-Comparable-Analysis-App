8fa4e6ff03f540e0a30c2d4a177feaf5d289566b
Matt Lee <mattlee@convene.com>
2021-10-28 15:11:10 -0400

    feat: Add firebase authentication api and start of sign up feature

--- src/Components/LogOnForm.js
+++ src/Components/LogOnForm.js
@@ -1,23 +1,51 @@
-import { useState, useRef } from "react";
+import { useRef, useState } from "react";
 
 const LogOnForm = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
+  const [isLogin, setIsLogin] = useState(false);
 
+  const changeAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
   const onSubmitHandler = (event) => {
     event.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
 
-    console.log(email, password);
+    if (isLogin) {
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAapwykxzCrhSRZM7sRNY4zz47rcBE-aDo",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((response) => {
+        if (response.ok) {
+          console.log("Success");
+        } else {
+          response.json().then((data) => {
+            console.log(data);
+          });
+        }
+      });
+    }
   };
   return (
     <form onSubmit={onSubmitHandler}>
       <div>
         <h3>Email</h3>
-        <input type="text" id="username" ref={emailRef} />
+        <input type="email" id="email" required ref={emailRef} />
         <h3>Password</h3>
-        <input type="text" id="password" ref={passwordRef} />
+        <input type="password" id="password" ref={passwordRef} />
       </div>
       <button type="submit" id="submit">
         <h3>Submit</h3>
