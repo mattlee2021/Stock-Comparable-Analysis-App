75e1c73d1d10354f1feb57d2d3d5d2d3698ad489
Matt Lee <mattlee@convene.com>
2022-06-14 16:29:49 -0400

    border outline on drag last element

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -30,13 +30,13 @@ const Card = (props) => {
       }
 
       const hoverBoundingRect = ref.current?.getBoundingClientRect(); //gets the rectangular border of the element picked up
-      console.log("hoverBoundingRect", hoverBoundingRect);
+      //console.log("hoverBoundingRect", hoverBoundingRect);
 
       const hoverMiddleY =
         (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
 
       const clientOffset = monitor.getClientOffset();
-      console.log("element clientOffset monitor", clientOffset);
+      //console.log("element clientOffset monitor", clientOffset);
 
       const hoverClientY = clientOffset.y - hoverBoundingRect.top;
 
@@ -64,76 +64,80 @@ const Card = (props) => {
 
   drag(drop(ref));
 
+  props.setIsDragging(isDragging);
+
   return (
-    <table ref={ref}>
-      <thead>
-        <tr>
-          <th className="metricLabel stockNames"> Metrics </th>
-          {stockData.map((stockData) => {
-            return <th className="stockNames"> {stockData["Name"]} </th>;
-          })}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th className="metricLabel">P/E</th>
-          {stockData.map((stockData) => {
-            return <td>{stockData["P/E"]}</td>;
-          })}
-        </tr>
-        <tr>
-          <th className="metricLabel">P/B</th>
-          {stockData.map((stockData) => {
-            return <td> {stockData["P/B"]} </td>;
-          })}
-        </tr>
-        <tr>
-          <th className="metricLabel">EV/EBITDA</th>
-          {stockData.map((stockData) => {
-            return <td> {stockData["EV/EBITDA"]} </td>;
-          })}
-        </tr>
-        <tr>
-          <th className="metricLabel">EV/Revenue</th>
-          {stockData.map((stockData) => {
-            return <td> {stockData["EV/Revenue"]} </td>;
-          })}
-        </tr>
-        <tr>
-          <th className="metricLabel">EPS</th>
-          {stockData.map((stockData) => {
-            return <td> {stockData["EPS"]} </td>;
-          })}
-        </tr>
-        <tr>
-          <th className="metricLabel">Profit Margin</th>
-          {stockData.map((stockData) => {
-            return <td> {stockData["Profit Margin"]} </td>;
-          })}
-        </tr>
-        <tr>
-          <th className="metricLabel">Sector</th>
-          {stockData.map((stockData) => {
-            return <td> {stockData["Sector"]} </td>;
-          })}
-        </tr>
-        <tr>
-          <td>&nbsp;</td>
-          {stockData.map((ticker, index) => {
-            return (
-              <td>
-                <DeleteButton
-                  ticker={ticker}
-                  tableNumber={props.tableNumber}
-                  stockData={stockData}
-                  setTableData={props.setTableData}
-                />
-              </td>
-            );
-          })}
-        </tr>
-      </tbody>
-    </table>
+    <div className={isDragging ? "dragging-border" : ""}>
+      <table ref={ref} className={props.className}>
+        <thead>
+          <tr>
+            <th className="metricLabel stockNames"> Metrics </th>
+            {stockData.map((stockData) => {
+              return <th className="stockNames"> {stockData["Name"]} </th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th className="metricLabel">P/E</th>
+            {stockData.map((stockData) => {
+              return <td>{stockData["P/E"]}</td>;
+            })}
+          </tr>
+          <tr>
+            <th className="metricLabel">P/B</th>
+            {stockData.map((stockData) => {
+              return <td> {stockData["P/B"]} </td>;
+            })}
+          </tr>
+          <tr>
+            <th className="metricLabel">EV/EBITDA</th>
+            {stockData.map((stockData) => {
+              return <td> {stockData["EV/EBITDA"]} </td>;
+            })}
+          </tr>
+          <tr>
+            <th className="metricLabel">EV/Revenue</th>
+            {stockData.map((stockData) => {
+              return <td> {stockData["EV/Revenue"]} </td>;
+            })}
+          </tr>
+          <tr>
+            <th className="metricLabel">EPS</th>
+            {stockData.map((stockData) => {
+              return <td> {stockData["EPS"]} </td>;
+            })}
+          </tr>
+          <tr>
+            <th className="metricLabel">Profit Margin</th>
+            {stockData.map((stockData) => {
+              return <td> {stockData["Profit Margin"]} </td>;
+            })}
+          </tr>
+          <tr>
+            <th className="metricLabel">Sector</th>
+            {stockData.map((stockData) => {
+              return <td> {stockData["Sector"]} </td>;
+            })}
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            {stockData.map((ticker, index) => {
+              return (
+                <td>
+                  <DeleteButton
+                    ticker={ticker}
+                    tableNumber={props.tableNumber}
+                    stockData={stockData}
+                    setTableData={props.setTableData}
+                  />
+                </td>
+              );
+            })}
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- src/Components/TableContainer.js
+++ src/Components/TableContainer.js
@@ -1,14 +1,18 @@
 import Card from "./Card";
 import update from "immutability-helper";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
+import "./TableContainer.scss";
 
 const TableContainer = (props) => {
   const { tableData, setTableData } = props;
+  const [isDragging, setIsDragging] = useState(false);
+
+  console.log("isDragging TableContainer", isDragging);
 
   const moveCard = useCallback(
     (dragIndex, hoverIndex) => {
-      console.log("dragIndex", dragIndex);
-      console.log("hoverIndex", hoverIndex);
+      // console.log("dragIndex", dragIndex);
+      // console.log("hoverIndex", hoverIndex);
       setTableData((prevCards) =>
         update(prevCards, {
           $splice: [
@@ -23,22 +27,37 @@ const TableContainer = (props) => {
 
   //console.log("tableData", tableData);
 
-  const renderCard = useCallback((stockData, index) => {
-    return (
-      <Card
-        key={index}
-        tableNumber={index}
-        stockData={stockData}
-        setTableData={setTableData}
-        moveCard={moveCard}
-      />
-    );
-  }, []);
+  // const renderCard = useCallback(
+  //   (stockData, index) => {
+  //     return (
+  //       <Card
+  //         className={isDragging ? "dragging-card" : ""}
+  //         key={index}
+  //         tableNumber={index}
+  //         stockData={stockData}
+  //         setTableData={setTableData}
+  //         moveCard={moveCard}
+  //         setIsDragging={setIsDragging}
+  //       />
+  //     );
+  //   },
+  //   [isDragging]
+  // );
 
   return (
     <>
-      <div>
-        {tableData.map((stockData, index) => renderCard(stockData, index))}
+      <div className={isDragging ? "dragging-border" : ""}>
+        {tableData.map((stockData, index) => (
+          <Card
+            className={isDragging ? "dragging-card" : ""}
+            key={index}
+            tableNumber={index}
+            stockData={stockData}
+            setTableData={setTableData}
+            moveCard={moveCard}
+            setIsDragging={setIsDragging}
+          />
+        ))}
       </div>
     </>
   );
